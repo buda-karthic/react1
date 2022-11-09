9ed9735b72a88c9043176a7159997653b2480970
karthic <buda.karthic@gmail.com>
2022-11-09 18:16:56 +0530

    adding more files

--- src/component/Animals2.js
+++ src/component/Animals2.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Animals4 } from "./Animals4";
 
 export default function Animals2(props) {
   return (
     <div>
+      <Animals4 name1={props.name1} />
       <div>{props.name}</div> <div>{props.name2}</div>
+      <p>The white and black coloured {props.name3}</p>
     </div>
   );
 }

07e10283f4deb92a9b98d9473337a07a26b23521
karthic <buda.karthic@gmail.com>
2022-11-09 18:16:41 +0530

    adding more files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import Animals from "../src/component/Animals";
 import Animals1 from "../src/component/Animals1";
 import Animals2 from "../src/component/Animals2";
+import { Animals4 } from "../src/component/Animals4";
 
 function App() {
   const ani = [
     {
       name: "Tiger",
-      type: "omnivores",
+      type: "Carnivores",
       color: "yellow",
     },
     {
@@ -20,13 +21,31 @@ function App() {
       type: "Herbivores",
       color: "White and Black",
     },
+    {
+      name: "Lion",
+      type: "Carnivores",
+      color: "Pale",
+    },
+    {
+      name: "Dog",
+      type: "Omnivores",
+      color: "Multi-color",
+    },
   ];
   return (
     <div className="App">
       <header className="App-header">
         <Animals />
+        *********
         <Animals1 name={"Hey this is Elephant"} />
-        <Animals2 name={ani[0].color} name2={ani[1].color} />
+        *******
+        <Animals2
+          name={ani[0].color}
+          name2={ani[1].color}
+          name3={ani[2].name}
+          name1={ani[3].name}
+        />
+        <Animals4 name1={ani[3].color} />
       </header>
     </div>
   );
